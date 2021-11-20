bd46e439772d57763042a9ece756e4da6d69c7f3
joli94 <adrian.apostol.94@gmail.com>
2021-11-20 14:14:59 +0200

    First commit

---
+++ src/main/java/ro/unibuc/dietapplication/DietApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.unibuc.dietapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/unibuc/dietapplication/DietApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.unibuc.dietapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
