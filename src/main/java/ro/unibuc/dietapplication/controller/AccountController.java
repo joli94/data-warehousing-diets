9c58555c9e1696c2f0b59677bca8a607945a9b1e
joli94 <adrian.apostol.94@gmail.com>
2021-11-22 23:03:23 +0200

    Added CRU operations for Account

---
+++ src/main/java/ro/unibuc/dietapplication/controller/AccountController.java
@@ -0,0 +1,44 @@
+package ro.unibuc.dietapplication.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.unibuc.dietapplication.dto.AccountDto;
+import ro.unibuc.dietapplication.exception.BadRequestException;
+import ro.unibuc.dietapplication.mapper.AccountMapper;
+import ro.unibuc.dietapplication.model.Account;
+import ro.unibuc.dietapplication.service.AccountService;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+    private final AccountService service;
+    private final AccountMapper mapper;
+
+    public AccountController(AccountService service, AccountMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> findById(@PathVariable Long id){
+        Account response = service.findById(id);
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> create(@RequestBody AccountDto request){
+        Account response = service.create(mapper.toEntity(request));
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountDto> update(@PathVariable Long id, @RequestBody AccountDto request){
+        if(id != request.getId()){
+            throw new BadRequestException("The path variable doesn't match the request body id!");
+        }
+
+        Account response = service.update(mapper.toEntity(request));
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/ro/unibuc/dietapplication/service/AccountService.java
@@ -0,0 +1,36 @@
+package ro.unibuc.dietapplication.service;
+
+import org.springframework.stereotype.Service;
+import ro.unibuc.dietapplication.model.Account;
+import ro.unibuc.dietapplication.repository.AccountRepository;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account findById(Long id){
+        return accountRepository.findById(id).orElseThrow(
+                ()-> new EntityNotFoundException("The account with this id doesn't exist in the database!")
+        );
+    }
+
+    public Account create(Account account){
+        return accountRepository.save(account);
+    }
+
+    public Account update(Account account){
+        if(accountRepository.existsById(account.getId())){
+            return accountRepository.save(account);
+        } else {
+            throw new EntityNotFoundException(String.format("There is no account with id=%s in the database!", account.getId().toString()));
+        }
+    }
+
+
+}
