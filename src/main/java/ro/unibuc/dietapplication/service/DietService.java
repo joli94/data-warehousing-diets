448423204dc62b0add8e6613a0916c595eec55b6
Andra Tomi <andratomi@Andras-MacBook-Air.local>
2021-11-30 01:25:14 +0200

    Add Diet controller and service

---
+++ src/main/java/ro/unibuc/dietapplication/service/DietService.java
@@ -0,0 +1,20 @@
+package ro.unibuc.dietapplication.service;
+
+import org.springframework.stereotype.Service;
+import ro.unibuc.dietapplication.model.Diet;
+import ro.unibuc.dietapplication.repository.DietRepository;
+import java.util.List;
+
+@Service
+public class DietService {
+
+    private final DietRepository dietRepository;
+
+    public DietService(DietRepository dietRepository) {
+        this.dietRepository = dietRepository;
+    }
+
+    public List<Diet> findAll(){
+        return dietRepository.findAll();
+    }
+}
