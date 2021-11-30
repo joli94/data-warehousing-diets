eb2390fe5417278f2bc85f74435dfac1572c764a
joli94 <adrian.apostol.94@gmail.com>
2021-11-30 21:37:48 +0200

    Added DietsByCategoryId

--- src/main/java/ro/unibuc/dietapplication/repository/WeightRepository.java
+++ src/main/java/ro/unibuc/dietapplication/repository/WeightRepository.java
@@ -3,5 +3,8 @@ package ro.unibuc.dietapplication.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.unibuc.dietapplication.model.Weight;
 
+import java.util.List;
+
 public interface WeightRepository extends JpaRepository <Weight, Long> {
+    List<Weight> findByUserId(Long id);
 }
