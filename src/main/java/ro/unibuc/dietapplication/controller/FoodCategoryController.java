3343b4055f46a339f733c71292bbe6a0d1bcb49e
joli94 <adrian.apostol.94@gmail.com>
2021-12-01 16:28:28 +0200

    Updated FoodCategoryController

--- src/main/java/ro/unibuc/dietapplication/controller/FoodCategoryController.java
+++ src/main/java/ro/unibuc/dietapplication/controller/FoodCategoryController.java
@@ -3,7 +3,6 @@ package ro.unibuc.dietapplication.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ro.unibuc.dietapplication.dto.DietTypeDto;
 import ro.unibuc.dietapplication.dto.FoodCategoryDto;
 import ro.unibuc.dietapplication.mapper.FoodCategoryMapper;
 import ro.unibuc.dietapplication.model.FoodCategory;
