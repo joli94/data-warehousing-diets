0361a9f5c723d96f35e4437765c5ec758908ebd2
joli94 <adrian.apostol.94@gmail.com>
2021-12-01 17:40:18 +0200

    Updated DietService

--- src/main/java/ro/unibuc/dietapplication/service/DietService.java
+++ src/main/java/ro/unibuc/dietapplication/service/DietService.java
@@ -1,6 +1,7 @@
 package ro.unibuc.dietapplication.service;
 
 import org.springframework.stereotype.Service;
+import ro.unibuc.dietapplication.exception.EntityNotFoundException;
 import ro.unibuc.dietapplication.model.Diet;
 import ro.unibuc.dietapplication.repository.DietRepository;
 import java.util.List;
@@ -21,4 +22,9 @@ public class DietService {
     public List<Diet> findByGoalId(Long id) { return dietRepository.findByDietGoalId(id); }
 
     public List<Diet> findByTypeId(Long id) { return dietRepository.findByDietTypeId(id); }
+
+    public Diet findById(Long id) { return dietRepository.findById(id).orElseThrow(
+            ()-> new EntityNotFoundException("The diet with this id doesn't exist in the database!")
+    );
+    }
 }
