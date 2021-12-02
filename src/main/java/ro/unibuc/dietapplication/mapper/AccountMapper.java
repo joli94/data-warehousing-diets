416b7068733ce181e5b91ededaeef9001058c181
joli94 <adrian.apostol.94@gmail.com>
2021-12-02 18:59:23 +0200

    Updated AccountMapper

--- src/main/java/ro/unibuc/dietapplication/mapper/AccountMapper.java
+++ src/main/java/ro/unibuc/dietapplication/mapper/AccountMapper.java
@@ -1,9 +1,20 @@
 package ro.unibuc.dietapplication.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import ro.unibuc.dietapplication.dto.AccountDto;
 import ro.unibuc.dietapplication.model.Account;
 
-@Mapper
-public interface AccountMapper extends EntityMapper<AccountDto, Account>{
+@Mapper(imports = {Account.class})
+public abstract class AccountMapper implements EntityMapper<AccountDto, Account>{
+    @Mappings({
+            @Mapping(target = "userId", source = "user.id")
+    })
+    public abstract AccountDto toDto(Account account);
+
+    @Mappings({
+            @Mapping(target = "user.id", source = "userId")
+    })
+    public abstract Account toEntity(AccountDto accountDto);
 }
