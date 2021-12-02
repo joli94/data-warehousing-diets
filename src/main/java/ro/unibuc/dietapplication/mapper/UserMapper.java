0fb7aa90911461c596c961fbe787388465f974ab
joli94 <2558817+joli94@users.noreply.github.com>
2021-12-02 19:55:15 +0200

    Add user flows (#12)
    * Updated User
    
    * Updated User
    
    * Updated User
    
    Co-authored-by: joli94 <adrian.apostol.94@gmail.com>

--- src/main/java/ro/unibuc/dietapplication/mapper/UserMapper.java
+++ src/main/java/ro/unibuc/dietapplication/mapper/UserMapper.java
@@ -1,9 +1,21 @@
 package ro.unibuc.dietapplication.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import ro.unibuc.dietapplication.dto.UserDto;
+import ro.unibuc.dietapplication.model.City;
 import ro.unibuc.dietapplication.model.User;
 
-@Mapper
-public interface UserMapper extends EntityMapper<UserDto, User>{
+@Mapper(imports = {City.class})
+public abstract class UserMapper implements EntityMapper<UserDto, User>{
+    @Mappings({
+            @Mapping(target = "cityId", source = "city.id")
+    })
+    public abstract UserDto toDto(User user);
+
+    @Mappings({
+            @Mapping(target = "city.id", source = "cityId")
+    })
+    public abstract User toEntity(UserDto userDto);
 }
