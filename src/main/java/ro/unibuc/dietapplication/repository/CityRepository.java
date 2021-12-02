7b4f02a6f932ff17cfc698025c610552cc6aad51
joli94 <adrian.apostol.94@gmail.com>
2021-12-02 18:33:24 +0200

    Updated CityRepository

--- src/main/java/ro/unibuc/dietapplication/repository/CityRepository.java
+++ src/main/java/ro/unibuc/dietapplication/repository/CityRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ro.unibuc.dietapplication.model.City;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository <City, Long> {
+    List<City> findByCountryId(Long id);
 }
