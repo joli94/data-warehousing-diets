f022b8408e54a3aaa7d9454a78073600c50e3321
joli94 <adrian.apostol.94@gmail.com>
2021-12-05 22:48:44 +0200

    Added IngredientRepository

---
+++ src/main/java/ro/unibuc/dietapplication/repository/IngredientRepository.java
@@ -0,0 +1,12 @@
+package ro.unibuc.dietapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ro.unibuc.dietapplication.model.Ingredient;
+
+import java.util.List;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    List<Ingredient> findByFoodId(Long id);
+}
